d48193b032c3305d8a1549e896503b4926f13dd9
alitinsky <alitinsky@gmail.com>
2018-02-15 15:31:18 +0300

    Fixed run_en_squad.sh

--- run_en_squad.sh
+++ run_en_squad.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=2 &&
-source ../virtualenv/en_squad/bin/activate &&
+source ./env/bin/activate &&
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/ &&
 nohup python3 squad_en_api.py > ./squad_en.log &
